cf82b7446ecf5998fdbd0291df4400a95faa7bfd
Nebojsa Lazarevic <lazarevic.nebojsa1005@gmail.com>
2021-03-13 15:38:26 +0100

    Changed styles for landing and header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,24 @@
 <template>
+<<<<<<< HEAD
     <header id="nav">
         <div class="container">
             <router-link tag="a" to="/">Home</router-link> 
             <router-link tag="a" to="/about">Contact me</router-link>
         </div>
+=======
+  <header id="nav">
+    <div class="container">
+      <router-link tag="a" to="/">Home</router-link>
+      <router-link tag="a" to="/about">Contact me</router-link>
+    </div>
+>>>>>>> a9cc423 (Changed styles for landing component)
   </header>
 </template>
 
 <style scoped>
 #nav {
   height: 50px;
+<<<<<<< HEAD
 
   background: white;
   display: flex;
@@ -19,6 +28,16 @@
   opacity: 0;
   animation: 1s nav-item 1.3s ease forwards;
  }
+=======
+  background: #002d62;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 150px 10px 20px;
+  opacity: 0;
+  animation: 1s nav-item 1.3s ease forwards;
+}
+>>>>>>> a9cc423 (Changed styles for landing component)
 
 @keyframes nav-item {
   100% {
@@ -32,23 +51,40 @@
   justify-content: space-around;
   align-items: center;
   width: 500px;
+<<<<<<< HEAD
   height: 100%
+=======
+  height: 100%;
+>>>>>>> a9cc423 (Changed styles for landing component)
 }
 
 #nav a {
   font-weight: bold;
   font-size: 25px;
+<<<<<<< HEAD
   color: #002D62;
+=======
+  color: white;
+>>>>>>> a9cc423 (Changed styles for landing component)
   text-decoration: none;
   transition: transform 0.3s ease;
 }
 
+<<<<<<< HEAD
 #nav a.router-link-exact-active {
   color: #002D62;
+=======
+#nav a:hover {
+  transform: scale(1.4);
+}
+
+#nav a.router-link-exact-active {
+>>>>>>> a9cc423 (Changed styles for landing component)
   transition: transform 0.3s ease;
   transform: scale(1.8);
 }
 
+<<<<<<< HEAD
 
 .slide-enter-active {
     animation: slide-in 0.2s forwards ease-in
@@ -72,6 +108,29 @@
 @media (max-width: 440px) {
     #nav a.router-link-exact-active {
     transform: scale(1.2)
+=======
+.slide-enter-active {
+  animation: slide-in 0.2s forwards ease-in;
+}
+.slide-leave-active {
+  animation: slide-in 0.3s forwards ease-in;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+
+@media (max-width: 440px) {
+  #nav a.router-link-exact-active {
+    transform: scale(1.2);
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
 }
 
@@ -82,6 +141,7 @@
   }
   .container {
     width: 60%;
+<<<<<<< HEAD
     text-align: left
 
   }
@@ -93,4 +153,15 @@
   }
 }
 
+=======
+    text-align: left;
+  }
+  #nav a {
+    font-size: 18px;
+  }
+  #nav a.router-link-exact-active {
+    transform: scale(1.4);
+  }
+}
+>>>>>>> a9cc423 (Changed styles for landing component)
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
     <div class="landing">
     <div class="left">
     <h1>Web Developer</h1>
@@ -20,12 +21,57 @@
     <i id="vue" class="fab fa-vuejs"></i>
     </div>
 </div>
+=======
+  <div class="wrapper">
+    <div class="landing">
+      <div class="left">
+        <h1>Web Developer</h1>
+
+        <div class="text">
+          <p>
+            My name is Nebojša Lazarević I come from Belgrade, Serbia, I`m a Web
+            Developer using JavaScript, HTML5 and CSS3 to create and design my
+            projects.
+          </p>
+          <p>
+            Using Vue.js as a framework, and Firebase Realtime Database I am
+            able to build user interfaces and single-page applications.
+          </p>
+          <p>
+            Udemy Academy student. Hard working and highly motivated, eager to
+            test my skills, and learn new technologies. Excelent in
+            problem-solving, passionate about software development. Excelent
+            spoken and written English
+          </p>
+          <p>
+            I`m a curious, self-driven individual and on the way to become best
+            of the best, write clean and reusable code, have an eye for details
+            and love to challenge myself.
+          </p>
+        </div>
+      </div>
+
+      <div class="right">
+        <div id="row">
+          <a class="cv-letter" :href="profile.cv">View CV</a>
+          <a class="cv-letter" :href="profile.cv">View Cover Letter</a>
+          <a :href="profile.gitHub" id="git-link">
+            Visit my GitHub page
+            <i id="git" class="fab fa-github"></i>
+          </a>
+        </div>
+        <i id="vue" class="fab fa-vuejs"></i>
+      </div>
+    </div>
+  </div>
+>>>>>>> a9cc423 (Changed styles for landing component)
 </template>
 
 <script>
 export default {
   computed: {
     profile() {
+<<<<<<< HEAD
       return this.$store.getters.profile
     }
   }
@@ -33,31 +79,65 @@ export default {
 </script>
 
 <style scoped>
+=======
+      return this.$store.getters.profile;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #002d62;
+  margin-bottom: 10px;
+}
+>>>>>>> a9cc423 (Changed styles for landing component)
 
 .landing {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 90%;
+<<<<<<< HEAD
   min-height: 100vh;
+=======
+>>>>>>> a9cc423 (Changed styles for landing component)
 }
 .left {
   width: 60%;
   height: 100%;
   text-align: left;
+<<<<<<< HEAD
   margin-top: 150px;
+=======
+  margin-top: 100px;
+>>>>>>> a9cc423 (Changed styles for landing component)
   animation: fall-in 0.5s 1s ease forwards;
   opacity: 0;
 }
 
 h1 {
   width: 90%;
+<<<<<<< HEAD
   margin-top: 20px;
   margin-bottom: 40px;
   font-size: 140px;
   font-weight: 900; 
   line-height: 1em;
   color: #002D62;
+=======
+  margin-top: 0px;
+  margin-bottom: 40px;
+  font-size: 140px;
+  font-weight: 900;
+  line-height: 1em;
+  color: white;
+>>>>>>> a9cc423 (Changed styles for landing component)
 }
 
 .text {
@@ -68,6 +148,7 @@ p {
   width: 90%;
   font-size: 26px;
   font-weight: 600;
+<<<<<<< HEAD
   color: #002D62;
 }
 
@@ -76,6 +157,15 @@ p {
   100% {
     margin-top: 50px;
     opacity: 1
+=======
+  color: white;
+}
+
+@keyframes fall-in {
+  100% {
+    margin-top: 50px;
+    opacity: 1;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
 }
 
@@ -107,6 +197,7 @@ p {
   justify-content: center;
 }
 
+<<<<<<< HEAD
 .cv-letter {
   background: #002D62;
   color: white;
@@ -119,10 +210,27 @@ p {
   margin: 10px;
   padding: 10px 20px;
   transition: all 0.3s ease
+=======
+a {
+  text-decoration: none;
+  font-size: 25px;
+  font-weight: 600;
+  transition: all 0.3s ease;
+  padding: 10px 20px;
+}
+
+.cv-letter {
+  background: transparent;
+  color: white;
+  text-align: center;
+  border-radius: 5px;
+  margin: 10px;
+>>>>>>> a9cc423 (Changed styles for landing component)
 }
 
 .cv-letter:hover {
   background: white;
+<<<<<<< HEAD
   color: #002D62;
 }
 
@@ -130,16 +238,44 @@ p {
   display: flex;
   align-items: center;
   justify-content: center;
+=======
+  color: #002d62;
+}
+
+#git-link {
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+}
+#git-link:hover {
+  background: white;
+  color: #002d62;
+>>>>>>> a9cc423 (Changed styles for landing component)
 }
 
 #git {
   padding: 0 0 0 10px;
   font-size: 60px;
+<<<<<<< HEAD
   color: #002D62;
 }
 
 #vue {
   color: #002D62;
+=======
+  color: white;
+  transition: color 0.3s ease;
+}
+
+#git-link:hover #git {
+  color: #002d62;
+}
+
+#vue {
+  color: white;
+>>>>>>> a9cc423 (Changed styles for landing component)
   font-size: 400px;
   padding: 50px;
 }
@@ -158,7 +294,11 @@ p {
 
 @media (max-width: 768px) {
   #landing {
+<<<<<<< HEAD
     height: 800px
+=======
+    height: 800px;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
   .left {
     padding-left: 0;
@@ -166,20 +306,34 @@ p {
   }
   .right {
     padding-bottom: 0px;
+<<<<<<< HEAD
     width: 50%
+=======
+    width: 50%;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
   h1 {
     font-size: 65px;
     width: 86%;
+<<<<<<< HEAD
     margin-bottom: 70px
   }
   p {
   
+=======
+    margin-bottom: 70px;
+  }
+  p {
+>>>>>>> a9cc423 (Changed styles for landing component)
     width: 100%;
   }
   #vue {
     font-size: 300px;
+<<<<<<< HEAD
     padding-bottom: 150px
+=======
+    padding-bottom: 150px;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
 }
 
@@ -195,16 +349,28 @@ p {
   .left {
     padding: 0 5px;
     width: 100%;
+<<<<<<< HEAD
     margin-top: 40px
+=======
+    margin-top: 40px;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
   .right {
     margin-top: 25px;
     padding-bottom: 30px;
+<<<<<<< HEAD
     width: 100%
   }
   .cv-letter  {
     font-size: 22px;
     margin-bottom: 10px
+=======
+    width: 100%;
+  }
+  .cv-letter {
+    font-size: 22px;
+    margin-bottom: 10px;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
   #git {
     font-size: 50px;
@@ -216,12 +382,20 @@ p {
     font-size: 60px;
     width: 84%;
     margin-bottom: 40px;
+<<<<<<< HEAD
     margin-left: 20px
+=======
+    margin-left: 20px;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
   p {
     font-size: 20px;
     width: 100%;
+<<<<<<< HEAD
     text-align: center
+=======
+    text-align: center;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
 }
 
@@ -231,10 +405,16 @@ p {
   }
 }
 
+<<<<<<< HEAD
 
 @media (max-width: 900px) and (max-height: 400px) {
   .landing {
     flex-direction: column
+=======
+@media (max-width: 900px) and (max-height: 400px) {
+  .landing {
+    flex-direction: column;
+>>>>>>> a9cc423 (Changed styles for landing component)
   }
   .left {
     width: 100%;
@@ -246,5 +426,8 @@ p {
     display: none;
   }
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> a9cc423 (Changed styles for landing component)
 </style>
